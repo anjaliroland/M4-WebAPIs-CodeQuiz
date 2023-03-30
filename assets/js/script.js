cd6ff09c1505a116ac3c5eb9e1e5a9aabb1ddbcf
Anjali Roland <anjaligroland@gmail.com>
2023-03-30 16:51:43 -0700

    made pseudocode for script

---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+// TODO: define all questions and answers
+    // create variables for questions and answers that connect to html elements
+
+// TODO: create timer function that will countdown seconds
+    // create variables for timer that connect to html elements
+    // timer function must:
+        // display correctly on screen
+        // subtract 20 seconds when incorrect answer is selected
+        // IF timer hits 0:
+            // End quiz function is called
+
+// TODO: create a function to start the quiz
+    // create variables for start that connect to html
+    // When quiz start button is pushed:
+        // hide the start button
+        // display the first question
+        // begin timer countdown
+
+// TODO: create a function to render a question
+    //create variables for game page to connect to html elements
+    // When question is rendered:
+        // remove previous question
+        // add question to question container
+        // make button for each answer
+        // add answers to answers container
+
+// TODO: create function to end quiz 
+    // create variables for end of quiz that connect to html elements
+    // when quiz is completed:
+        // timer stops
+        // questions are hidden
+        // end screen is shown
+        // time remaining is shown as score
+
+// TODO: create function that handles answer responses
+    // create variables for answer responses that connect to html elements
+    // IF answer is correct :
+        // hide answered question
+        // display new question
+    // ELSE IF answer is wrong:
+        // subtract time from timer
+        // wrong answer message is flashed on screen (setTimeout)
+        // hide answered question
+        // display new question
+    // 
+    // IF no more questions: 
+        // end quiz is called
+    
+// TODO: create a function that saves high scores
+    // create variables for scores that connect to html elements
+    // when function runs:
+        // get value of users initials
+        // validate input
+        // retrieve existing data from local storage
+        // redirect to start screen after save
+
+// TODO: create a function that listens for keyboard events
+    // checks if the 'Enter' key was pressed for saving scores
+    // POTENTIALLY check if 'a', 'b', 'c', or 'd' is pressed for answers
+
+// TODO: add event listeners for the following: 
+    // click start
+    // click answers
+    // click save score
+    // click view scores
+
+
+
